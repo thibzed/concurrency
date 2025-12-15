ef90df610f73a4eaef6ff98d486e873a5a3f1a75
thibzed <thibaultedouardvh@gmail.com>
2025-12-15 16:29:40 +0100

    test with a large dataset

--- src/map_reduce.cpp
+++ src/map_reduce.cpp
@@ -1,8 +1,9 @@
 #include "../include/MapReduce.hpp"
+#include <numeric>
 
 int main(){
-    size_t nb_threads = 4;
-    MapReduce <int,int> mr(nb_threads);
+    size_t nb_threads = 8;
+    MapReduce <int,double> mr(nb_threads);
 
     //std::cout << "10 elem, 4 threads :" << std::endl;
     //for (size_t i = 0 ; i < nb_threads ; i++){
@@ -16,9 +17,11 @@ int main(){
     //    std::cout << "Thread " << i << " : [" << start << ", " << end << "[" << std::endl;
     //}
 
-    std::vector<int> data = {1, 2, 3, 4, 5 , 6, 7, 8, 9, 10};
+    size_t data_size = 10'000'000;
+    std::vector<int> data (data_size);
+    std::iota(data.begin(), data.end(), 0);
     auto map_fn = [](int x){return x*x;};
-    auto reduce_fn = [](int x, int y){return x + y;};
+    auto reduce_fn = [](double x, double y){return x + y;};
     //int result = mr.compute_par(data, map_fn, reduce_fn, 0);
     //std::cout << "result : " << result;
     mr.benchmark(data, map_fn, reduce_fn, 0);
